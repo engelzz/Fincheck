e10aad194ac75f16746546d38cdff35c464e0945
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2024-07-25 15:46:38 -0300

    feat: login screen ui

--- frontend/src/ui/pages/Login/login.tsx
+++ frontend/src/ui/pages/Login/login.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { useLoginController } from "./useLoginController";
 
 export function Login() {
+  const { handleSubmit, register, errors, isPending } = useLoginController();
+
   return (
     <>
       <header className="flex flex-col items-center gap-4 text-center">
@@ -22,20 +25,25 @@ export function Login() {
         </p>
       </header>
 
-      <form className="mt-[60px] flex flex-col gap-4">
+      <form
+      className="mt-[60px] flex flex-col gap-4"
+      onSubmit={handleSubmit}
+      >
         <Input
           type="email"
           placeholder="E-mail"
-          name="email"
+          error={errors.email?.message}
+          {...register('email', )}
         />
 
         <Input
           type="password"
           placeholder="Senha"
-          name="password"
+          error={errors.password?.message}
+          {...register('password', )}
         />
 
-        <Button type="submit" className="mt-2">
+        <Button type="submit" className="mt-2" isPending={isPending}>
           Entrar
         </Button>
       </form>
