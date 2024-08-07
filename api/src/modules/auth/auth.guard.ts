86efb06cbbed29407ac9ed7a513c2065ef70f91e
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2024-08-07 12:14:54 -0300

    ci: errors message

--- api/src/modules/auth/auth.guard.ts
+++ api/src/modules/auth/auth.guard.ts
@@ -28,7 +28,7 @@ export class AuthGuard implements CanActivate {
     const token = this.extractTokenFromHeader(request);
 
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Missing Token');
     }
 
     try {
@@ -38,7 +38,7 @@ export class AuthGuard implements CanActivate {
 
       request['userId'] = payload.sub;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid Token');
     }
 
     return true;
