bf40c2c2438a7f77ba5a5c96216b1f859d7e5458
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2024-08-12 15:25:35 -0300

    docs: tailwind constants

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,6 +21,7 @@ export default {
         },
 
         red: {
+          0: '#F0F5F5',
           50: '#FFF5F5',
           100: '#FFE3E3',
           200: '#FFC9C9',
@@ -175,6 +176,33 @@ export default {
           800: '#E8590C',
           900: '#D9480F',
         },
+      },
+      keyframes: {
+        slideUpAndFade: {
+          from: { opacity: '0', transform: 'translateY(2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        overlayShow: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+        contentShow: {
+          from: { opacity: '0', transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+        },
+      },
+      animation: {
+        'slide-up-and-fade': 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'slide-down-and-fade': 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'overlay-show': 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'content-show': 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
       }
     },
   },
