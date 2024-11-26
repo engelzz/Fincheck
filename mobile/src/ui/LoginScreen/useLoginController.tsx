077b3d62effe3e7713a82604a4269e4603f9ce43
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2024-11-26 17:51:20 -0300

    feat: transactions and register controllers

---
+++ mobile/src/ui/LoginScreen/useLoginController.tsx
@@ -0,0 +1,51 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { authService } from '../../app/services/AuthService/auth.Service.ts';
+import { SigninParams } from '../../app/services/AuthService/signin';
+import { useAuth } from '../../hooks/useAuth';
+
+
+
+const schema = z.object({
+  email: z.string()
+    .nonempty('E-mail é obrigatório')
+    .email('Informe um e-mail válido'),
+  password: z.string()
+    .nonempty('Senha é obrigatória')
+    .min(8, 'Senha deve conter pelo menos 8 dígitos'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useLoginController() {
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const { mutateAsync, isLoading } = useMutation({
+    mutationFn: async (data: SigninParams) => {
+      return authService.signin(data);
+    },
+  });
+
+  const { signin } = useAuth();
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try {
+      const { accessToken } = await mutateAsync(data);
+
+      signin(accessToken);
+    } catch {
+      console.log('Credenciais inválidas!')
+    }
+  });
+
+  return { handleSubmit, register, errors, isLoading, control};
+}
---
+++ mobile/src/ui/RegisterScreen/useRegisterController.tsx
@@ -0,0 +1,52 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { authService } from "../../app/services/AuthService/auth.Service.ts";
+import { SignupParams } from "../../app/services/AuthService/signup";
+import { useAuth } from "../../hooks/useAuth";
+
+
+
+const schema = z.object({
+  name: z.string().nonempty('Nome é obrigatório'),
+  email: z.string()
+    .nonempty('E-mail é obrigatório')
+    .email('Informe um e-mail válido'),
+  password: z.string()
+    .nonempty('Senha é obrigatória')
+    .min(8, 'Senha deve conter pelo menos 8 dígitos'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useRegisterController() {
+  const {
+    handleSubmit: hookFormSubmit,
+    register,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const { mutateAsync, isLoading } = useMutation({
+    mutationFn: async (data: SignupParams) => {
+      return authService.signup(data);
+    },
+  });
+
+  const { signin } = useAuth();
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try {
+      const { accessToken } = await mutateAsync(data);
+
+      signin(accessToken);
+    } catch {
+      console.log('Ocorreu um erro ao criar a sua conta!')
+    }
+  });
+
+  return { register, errors, handleSubmit, isLoading, control };
+}
