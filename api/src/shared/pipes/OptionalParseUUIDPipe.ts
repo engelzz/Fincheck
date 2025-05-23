044e6c1bf7fd31712a5ca66c46e2f316e03d4eed
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2025-05-23 12:11:42 -0300

    refac: api refac

--- api/src/shared/pipes/OptionalParseUUIDPipe.ts
+++ api/src/shared/pipes/OptionalParseUUIDPipe.ts
@@ -1,8 +1,10 @@
 import { ArgumentMetadata, ParseUUIDPipe } from '@nestjs/common';
 
 export class OptionalParseUUIDPipe extends ParseUUIDPipe {
-  async transform(value: string, metadata: ArgumentMetadata): Promise<string> {
-    if (typeof value === 'undefined') return undefined;
+  override async transform(value: string, metadata: ArgumentMetadata) {
+    if (typeof value === 'undefined') {
+      return undefined;
+    }
 
     return super.transform(value, metadata);
   }
