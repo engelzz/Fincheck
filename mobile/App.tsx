f3bace3c0633e88e51310688bae5c97f0c30c517
Matheus Engel <matheusribeiroengel@outlook.com>
2026-05-21 21:53:40 -0300

    fix: update app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,17 @@
-import { useFonts } from 'expo-font';
-import { AuthNavigation } from './src/app/auth/AuthNavigation';
+import { useFonts } from "expo-font";
+import Toast from "react-native-toast-message";
+import { AuthNavigation } from "./src/app/auth/AuthNavigation";
+import { toastConfig } from "./src/ui/components/Toasts/toastConfig";
+
+if (__DEV__) {
+  require("./ReactotronConfig");
+}
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
-    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
-    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
-    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-700": require("./src/assets/fonts/GeneralSans-Bold.otf"),
   });
 
   if (!isFontsLoaded) {
@@ -13,6 +19,9 @@ export default function App() {
   }
 
   return (
-    <AuthNavigation />
+    <>
+      <AuthNavigation />
+      <Toast config={toastConfig} visibilityTime={2500} />
+    </>
   );
 }
